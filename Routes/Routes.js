7fd24ff51a1855385e33420bc07fc6fc7b8b25dc
abhishek lohani <alohani1308@gmail.com>
2023-06-14 01:24:24 +0530

    API

---
+++ Routes/Routes.js
@@ -0,0 +1,59 @@
+const express=require('express');
+const Model=require('../Models/Models')
+
+const router=express.Router();
+
+//Post Method
+
+    router.post('/post', async (req, res) => {
+        const data = new Model({
+            name: req.body.name,
+            age: req.body.age
+        })
+    
+        try {
+            const dataToSave = await data.save();
+            res.status(200).json(dataToSave)
+        }
+        catch (error) {
+            res.status(400).json({message: error.message})
+        }
+    })
+
+
+
+//Get all Method
+router.get('/getAll',async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+
+    router.get('/getOne/:id', async (req, res) => {
+        try{
+            const data = await Model.findById(req.params.id);
+            res.json(data)
+        }
+        catch(error){
+            res.status(500).json({message: error.message})
+        }
+    })
+
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+module.exports=router;
--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const express=require('express');
+require('dotenv').config();
 
-const app=express();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes=require('./Routes/Routes');
 
-app.get('/',(req,res)=>{
-    res.send("Home Page");
-});
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+app.use(express.json());
+app.use('/',routes);
 
 
-app.listen(8000,()=>{
-    console.log("Server is running................!");
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
 })
